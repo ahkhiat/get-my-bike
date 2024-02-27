22f60edf67e00c22d827407d9dc7793c4a647b76
tleung <ahkhiat@hotmail.com>
2024-02-27 16:56:42 +0100

    webpack encore pas ok

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -22,7 +22,7 @@ class DashboardController extends AbstractDashboardController
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
-        return parent::index();
+        // return parent::index();
 
         // Option 1. You can make your dashboard redirect to some common page of your backend
         //
@@ -38,7 +38,7 @@ class DashboardController extends AbstractDashboardController
         // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
         // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
         //
-        // return $this->render('admin/index.html.twig');
+        return $this->render('admin/index.html.twig');
 
        
 
